7ac35688fb2ab1a69ce5fb2bca4f612542e17af4
DavyAndrade <davyandrade.dev@gmail.com>
2026-01-10 22:26:57 -0300

    feat: adiciona suporte ao estilo transparente no componente Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,7 @@
 import type { ButtonHTMLAttributes, ReactNode } from "react";
 
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "transparent";
   fullWidth?: boolean;
   className?: string;
   children?: ReactNode;
@@ -15,9 +14,13 @@ const Button = ({
   children,
   ...props
 }: ButtonProps) => {
+  const baseStyles =
+    "rounded-md hover:cursor-pointer transition-colors p-2 font-bold";
+
   const variantStyles = {
     primary: "bg-blue-500 text-white hover:bg-blue-600",
     secondary: "bg-gray-500 text-white hover:bg-gray-600",
+    transparent: "bg-transparent hover:bg-gray-700",
   };
 
   if (fullWidth) {
@@ -26,7 +29,11 @@ const Button = ({
     className = `${variantStyles[variant]} ${className}`;
   }
 
-  return <button {...props} className={className}>{children}</button>;
+  return (
+    <button {...props} className={`${baseStyles} ${className}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
