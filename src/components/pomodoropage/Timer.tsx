09229473e6c9d0703137194c313da3e7731fa45d
DavyAndrade <davy.dama55@gmail.com>
2026-01-11 19:01:01 -0300

    feat: base da interface de TImer e Pomodoro.

--- src/components/pomodoropage/Timer.tsx
+++ src/components/pomodoropage/Timer.tsx
@@ -1,3 +1,23 @@
+import { Heading, ProgressBar } from "../ui";
+
 export default function Timer() {
-  return <article>Timer Component</article>;
+  return (
+    <article className="flex flex-col w-full gap-4 justify-center items-center">
+      <Heading as="h2">25:00</Heading>
+
+      <ProgressBar progress={1} />
+
+      <div className="w-full grid grid-cols-3 gap-2">
+        <button className="bg-green-600 hover:bg-green-500 text-white px-4 py-2 rounded-md transition-colors">
+          Reiniciar
+        </button>
+        <button className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded-md transition-colors">
+          Iniciar/Pausar
+        </button>
+        <button className="bg-yellow-600 hover:bg-yellow-500 text-white px-4 py-2 rounded-md transition-colors">
+          Pular
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/pages/Pomodoro.tsx
+++ src/pages/Pomodoro.tsx
@@ -1,6 +1,8 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useTasks } from "../hooks/useTasks";
 import { MoveLeft } from "lucide-react";
+import { Button, Heading, Text } from "../components/ui";
+import Timer from "../components/pomodoropage/Timer";
 
 export default function Pomodoro() {
   const { taskId } = useParams<{ taskId: string }>();
@@ -11,38 +13,54 @@ export default function Pomodoro() {
 
   if (!task) {
     return (
-      <main className="flex text-white">
-        <p>Tarefa não encontrada</p>
-      </main>
+      <div className="flex min-h-full justify-center items-center gap-4">
+        <Button
+          onClick={() => navigate("/")}
+          className="text-left flex items-center gap-2 text-gray-400 hover:text-white transition-colors hover:cursor-pointer w-fit"
+          variant="primary"
+          size="lg"
+        >
+          <MoveLeft size={18} />
+          Voltar para Tarefas
+        </Button>
+        <Heading
+          as="h1"
+          className="flex min-h-full justify-center items-center flex-2"
+        >
+          Tarefa não encontrada
+        </Heading>
+      </div>
     );
   }
 
   return (
-    <main className="w-full bg-gray-800 text-white flex flex-col py-16 px-4 items-center justify-center">
-      <div className="flex flex-col w-full md:max-w-3xl gap-8">
-        <button
-          onClick={() => navigate("/")}
-          className="text-left flex items-center gap-2 text-gray-400 hover:text-white transition-colors hover:cursor-pointer w-fit"
-        >
-          <MoveLeft size={18} />
-          Voltar para Tarefas
-        </button>
-        <section className="w-full flex flex-col justify-center items-center bg-gray-700/50 p-8 rounded-lg shadow-xl gap-4">
-          <div className="w-full flex flex-col justify-center items-center gap-1">
-            <h1 className="text-xl font-bold">{task.title}</h1>
-            {task.description && (
-              <p className="text-gray-400 text-center w-[75ch]">
-                {task.description}
-              </p>
-            )}
-          </div>
+    <>
+      <Button
+        onClick={() => navigate("/")}
+        className="text-left flex items-center gap-2 text-gray-400 hover:text-white transition-colors hover:cursor-pointer w-fit"
+        variant="transparent"
+      >
+        <MoveLeft size={18} />
+        Voltar para Tarefas
+      </Button>
 
-          <p className="text-gray-400">
-            {task.pomodorosCompleted} / {task.estimatedPomodoros} pomodoros
-            completados
-          </p>
-        </section>
-      </div>
-    </main>
+      <section className="w-full grid grid-cols-1 justify-center items-center bg-gray-700/50 p-8 rounded-lg shadow-xl gap-4">
+        <div className="w-full flex flex-col justify-center items-center gap-1">
+          <Heading as="h1">{task.title}</Heading>
+          {task.description && (
+            <Text className="text-gray-400 text-center w-[75ch]">
+              {task.description}
+            </Text>
+          )}
+        </div>
+
+        <Text variant="muted" className="text-gray-400 text-center">
+          {task.pomodorosCompleted} / {task.estimatedPomodoros} pomodoros
+          completados
+        </Text>
+
+        <Timer />
+      </section>
+    </>
   );
 }
