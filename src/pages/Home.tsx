86de06a04b513b32f049414f2451d14c0f9a5517
DavyAndrade <davy.dama55@gmail.com>
2026-01-11 18:33:37 -0300

    homepage with new ui components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,37 @@
 import { EllipsisVertical } from "lucide-react";
 import TaskCard from "../components/homepage/TaskCard";
 import { useTasks } from "../hooks/useTasks";
-import TaskForm from "../components/homepage/AddTaskForm";
 import { useEffect } from "react";
+import { Button, Heading, Text } from "../components/ui";
+import AddTaskForm from "../components/homepage/AddTaskForm";
 
 export default function Home() {
   const { tasks, addTask, editTask, toggleComplete, removeTask } = useTasks();
 
   useEffect(() => {
     document.title = "Pomotimer - Tarefas";
-  }, [])
+  }, []);
 
   return (
     <section className="w-full flex flex-col justify-between items-center gap-8">
       {/* Cabeçalho */}
       <div className="flex justify-between w-full border-b-2 border-gray-600 pb-4">
-        <h2 className="text-xl font-bold">Tarefas</h2>
-        <button className="flex justify-center items-center p-1 bg-gray-700 rounded-md hover:bg-gray-600 hover:cursor-pointer transition-colors">
+        <Heading as="h4" className="">
+          Tarefas
+        </Heading>
+        <Button
+          size="icon"
+          variant="transparent"
+          className="flex justify-center items-center p-1 bg-gray-700 rounded-md hover:bg-gray-600 hover:cursor-pointer transition-colors"
+        >
           <EllipsisVertical size={20} />
-        </button>
+        </Button>
       </div>
-
       {/* Container de Tarefas */}
       <div className="w-full flex flex-col justify-center items-center gap-8">
         {/* Botão de Adicionar Tarefa */}
 
-        <TaskForm onAddTask={addTask} />
+        <AddTaskForm onAddTask={addTask} />
 
         {/* Lista de Tarefas */}
 
@@ -40,13 +46,13 @@ export default function Home() {
             />
           ))
         ) : (
-          <div>
-            <p className="text-gray-500 text-center text-lg">
+          <div className="grid gap-1">
+            <Text variant="muted" className="text-center text-lg">
               Nenhuma tarefa adicionada ainda.
-            </p>
-            <p className="text-gray-500 text-center text-lg">
+            </Text>
+            <Text variant="muted" className="text-center text-lg">
               Comece adicionando sua primeira tarefa acima!
-            </p>
+            </Text>
           </div>
         )}
       </div>
